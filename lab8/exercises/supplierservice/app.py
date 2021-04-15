87b2fb3aceb5d1b347cf79f65bd2d274cf89d05b
IndikaKuma <ipkumarawd@yahoo.com>
2021-04-15 22:20:42 +0200

    Update Supplier Service

--- lab8/exercises/supplierservice/app.py
+++ lab8/exercises/supplierservice/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/supplier1/orders/', methods=['POST'])
+@app.route('/supplier1/orders/', methods=['POST', 'GET'])
 def get_order():
     data = request.get_json(silent=True)
     return jsonify(data), 200
